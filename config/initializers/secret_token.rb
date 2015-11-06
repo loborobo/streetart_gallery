851098169f74d411bace1a9c1a288f42b4310739
loborobo <rlobopires@gmail.com>
2015-11-06 15:29:56 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+StreetartGallery::Application.config.secret_key_base = 'a1b8902fd0824126f07b9f14350cf555f81ab1a2547fb6d2c68cf5d427613621497c82420250b783cd1fcdea8bb87cba10a6fd0b9c86f2b307027d9ab227f289'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+StreetartGallery::Application.config.session_store :cookie_store, key: '_streetart_gallery_session'
